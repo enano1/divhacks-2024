a340496d010182077b6a7686445e5880e13794be
Paul Martin Enano <paul.m.enano@gmail.com>
2024-10-05 13:40:48 -0400

    Resolved merge conflicts

--- divhacksproject/settings.py
+++ divhacksproject/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'divhacksproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'divhacksproject',  # Correct database name
+        'CLIENT': {
+            'host': 'mongodb+srv://paulmartinenano:p3LoPMCLLXjbIIby@project.azd3w.mongodb.net/divhacksproject?retryWrites=true&w=majority',  # Ensure the database name is in the URI
+            'username': 'paulmartinenano',
+            'password': 'p3LoPMCLLXjbIIby',
+            'authSource': 'admin',
+            'authMechanism': 'SCRAM-SHA-1'
+        }
     }
 }
 
--- project/models.py
+++ project/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+# In project/models.py (assuming your app is named 'project')
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    client_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)  # Automatically generates unique UUID for security
+    email = models.EmailField(unique=True)
+    resources = models.JSONField() # To store resource-related information for calculations
+
+    def __str__(self):
+        return self.name
