d1aa280a99f7972db0ab9134f6b19459d7e9cd66
Paul Martin Enano <paul.m.enano@gmail.com>
2024-10-05 12:35:30 -0400

    added templates views urls.py

---
+++ project/urls.py
@@ -0,0 +1,9 @@
+# Url Patterns for the project!!
+
+from django.urls import path
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    # path('', views.home, name="home"),
+]
--- project/views.py
+++ project/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import *
+
+####################################################################
+################## PUT A CLASS HERE FOR THE MODELS ################# or figure out how to import mongodb into django LOL
+####################################################################
+
