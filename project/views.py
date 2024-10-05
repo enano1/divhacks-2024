4197405213bc82942f2e9c18d0cbb3c27ff47ea9
VictorRobila <victorrobila@hunterschools.org>
2024-10-05 15:20:38 -0400

    front end stuff
    front end stuff

--- project/views.py
+++ project/views.py
@@ -12,3 +12,35 @@ def home(request):
     template_name = 'project/home.html'
 
     return render(request, template_name)
+def dashboard(request):
+    '''
+    View for the home page
+    '''
+
+    template_name = 'project/dashboard.html'
+
+    return render(request, template_name)
+def application(request):
+    '''
+    View for the home page
+    '''
+
+    template_name = 'project/application.html'
+
+    return render(request, template_name)
+def kpichooser(request):
+    '''
+    View for the home page
+    '''
+
+    template_name = 'project/kpichooser.html'
+
+    return render(request, template_name)
+def loantracker(request):
+    '''
+    View for the home page
+    '''
+
+    template_name = 'project/loantracker.html'
+
+    return render(request, template_name)
