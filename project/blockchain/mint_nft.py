d14d98ef417ec4dac317f28c7a40be592406d6c9
andeliuliu <andelerliutv@gmail.com>
2024-10-06 09:30:47 -0400

    NFT WORKS

--- project/blockchain/mint_nft.py
+++ project/blockchain/mint_nft.py
@@ -13,8 +13,9 @@ def send_nft(loan_statement):
     }
     data = {
         "quantity": "1",
-        "chain": "sepolia",
-        "contractAddress": "0xCaedFe8d392A1679a92B755D0F7b5C68fc2aaAe4",
+        "chain": "polygon",
+        "contractAddress": "0x4C293079F021FF1e4DeBA6396a9C2D22D3068B61",
+        "recipientAddress": "0xd33dE88B94a56544034bc8c829078eba5DbF68f8",
         "data": loan_statement
     }
 
@@ -26,32 +27,30 @@ def send_nft(loan_statement):
 
     return
 
-send_nft("sup")
+# url = "https://api.verbwire.com/v1/nft/deploy/deployContract"
 
-url = "https://api.verbwire.com/v1/nft/deploy/deployContract"
+# # Headers with the Verbwire API key
+# headers = {
+#     "accept": "application/json",
+#     "X-API-Key": "sk_live_b7624d12-b104-4281-91c2-5dcfdbcdbf98", 
+#     "Content-Type": "application/json"
+# }
 
-# Headers with the Verbwire API key
-headers = {
-    "accept": "application/json",
-    "X-API-Key": "sk_live_b7624d12-b104-4281-91c2-5dcfdbcdbf98", 
-    "Content-Type": "application/json"
-}
+# # Data required to deploy the contract
+# data = {
+#     "chain": "polygon",  # Specify the blockchain you want to deploy to, e.g., 'sepolia', 'goerli', etc.
+#     "contractName": "MyNFTCollection",  # Name of your NFT collection
+# }
 
-# Data required to deploy the contract
-data = {
-    "chain": "goerli",  # Specify the blockchain you want to deploy to, e.g., 'sepolia', 'goerli', etc.
-    "contractName": "MyNFTCollection",  # Name of your NFT collection
-}
+# # Sending POST request to deploy the contract
+# response = requests.post(url, json=data, headers=headers)
 
-# Sending POST request to deploy the contract
-response = requests.post(url, json=data, headers=headers)
-
-# Check the response
-if response.status_code == 200:
-    response_data = response.json()
-    print("Contract deployed successfully:")
-    print(f"Contract Address: {response_data.get('contractAddress')}")
-    print(f"Transaction Hash: {response_data.get('transactionHash')}")
-else:
-    print("Failed to deploy contract:")
-    print(response.text)
+# # Check the response
+# if response.status_code == 200:
+#     response_data = response.json()
+#     print("Contract deployed successfully:")
+#     print(f"Contract Address: {response_data.get('contractAddress')}")
+#     print(f"Transaction Hash: {response_data.get('transactionHash')}")
+# else:
+#     print("Failed to deploy contract:")
+#     print(response.text)
