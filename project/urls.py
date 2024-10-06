978b39ceae635c57a1c8721d92ffe8fceaf17f57
Paul Martin Enano <paul.m.enano@gmail.com>
2024-10-05 23:15:10 -0400

    added stuff

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name="home"),
+    path('', views.home, name="home"),
 
     path('dashboard/', views.dashboard, name="dashboard"),
     path('application/', views.application, name="application"),
