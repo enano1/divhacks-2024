72061c07a25d50bbf93dcf9736948111beb8ff3b
Paul Martin Enano <paul.m.enano@gmail.com>
2024-10-06 10:07:55 -0400

    deployment variables

--- divhacksproject/settings.py
+++ divhacksproject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-gn$8%%02q7=cm%q=lmja5(aa4h@tsij#o0^f8idgsvxj^-8xq$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'https://divhacks-2024.vercel.app/',]
 
 
 # Application definition
