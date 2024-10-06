ead35eb9cfa209aa1cba1b2608d82a3d6915a6a0
Paul Martin Enano <paul.m.enano@gmail.com>
2024-10-05 23:26:20 -0400

    added stuff

--- divhacksproject/settings.py
+++ divhacksproject/settings.py
@@ -136,3 +136,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # This tells Django where
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
--- project/urls.py
+++ project/urls.py
@@ -3,9 +3,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', views.home, name="home"),
+    
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('dashboard/', views.dashboard, name="dashboard"),
     path('application/', views.application, name="application"),
